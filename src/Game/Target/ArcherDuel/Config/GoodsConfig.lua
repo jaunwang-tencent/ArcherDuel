216417597ec989eaeaba6054073dc609dd0ddc91
jaunwang <jaunwang@tencent.com>
2025-09-10 20:26:31 +0800

    配置

--- src/Game/Target/ArcherDuel/Config/GoodsConfig.lua
+++ src/Game/Target/ArcherDuel/Config/GoodsConfig.lua
@@ -1,6 +1,18 @@
 --这里需要设计商品配置
 local GoodsConfig = {
-
+    [1] = {
+        ID = 1,
+        -- 商品类别，用于区分索引的config
+        Type = "Equipment",
+        -- 对应config编号
+        Index = 1,
+        -- 价格（钻石）
+        Price = 200,
+        -- 限购次数
+        Times = 2,
+        -- 权重
+        Weight = 10
+    }
 }
 
 return GoodsConfig
