121eff68a074fbbf7361074658f1043da9180452
easonwwang <easonwwang@tencent.com>
2025-09-10 13:17:10 +0800

    --简单的任务系统  任务重置和添加存取

---
+++ src/Game/Framework/Task/TaskPool.lua
@@ -0,0 +1,35 @@
+
+local TaskPool = {}
+
+local  TaskClass = UGCS.Framework.TaskManager.Task
+
+local TaskEvents = require("Game.Framework.Task.TaskEvents")
+
+TaskPool.taskcfg = {
+    { id = 1, taskName = "使用弓箭赢得2场比赛", taskDes = "使用弓箭赢得2场比赛",  condition = {type = TaskEvents.BattleWin , param = {2} },  award = {exp = 5} },
+    { id = 2, taskName = "战斗爆头2次", taskDes = "战斗爆头2次",  condition =  {TaskEvents.HeadShot , param = {2} },  award = {exp = 10} },
+    { id = 3, taskName = "登录游戏1次", taskDes = "登录游戏1次",  condition =  {TaskEvents.LoginGame , param = {} },  award = {exp = 15} }
+}
+
+function TaskPool.BuildTask()
+
+    local  TaskManagerInstance = UGCS.Framework.TaskManager:GetInsatnce()
+    for k, v in pairs(TaskPool.taskcfg) do
+
+        local condition = TaskManagerInstance:createCondition(v.condition.type, {
+            requiredAmount = v.condition.param[1]
+        })
+
+        local Quest = TaskClass:new(v.id, v.taskName, v.taskDes, {condition}, v.award)
+        Quest:addOnCompleteCallback(function()
+            -- Log:PrintLog("恭喜你完成了任务!")
+        end)
+        TaskManagerInstance:addTask(Quest)
+        TaskManagerInstance:acceptTask(Quest.id)
+    end
+    TaskManagerInstance:LoadSavedTaskData()
+
+    return TaskManagerInstance
+end
+
+return TaskPool
